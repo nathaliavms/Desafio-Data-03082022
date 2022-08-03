90ee99536628aa6fe72e611bc4e7838765a714a9
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-03 16:30:37 -0300

    Desafio

---
+++ DesafioData03082022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesafioData03082022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            int B = 9;
+            string X = "X = ";
+
+            Console.Write(X);
+            Console.WriteLine(A + B);
+        }
+    }
+}
